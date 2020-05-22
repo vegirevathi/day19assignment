d9e8715525fb0ceec59bda59dd0f605d43dc99ba
vegirevathi <vegirevathi@gmail.com>
2020-05-22 17:51:50 +0530

    including max working hrs condition

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,5 +1,11 @@
 public class EmpWageBuilde{
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation");
+		int i, NumofWorkingHrs=100, totalWage=0, WagePerHr=20;
+		for (i=0; i<= NumofWorkingHrs; i++)
+		{
+			totalWage = WagePerHr*i;
+			 System.out.println("total wage " +totalWage);
 		}
 	}	
+}
