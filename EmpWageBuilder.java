3af9edc98aae038637722a20c7a2a6ab66fc9454
vegirevathi <vegirevathi@gmail.com>
2020-05-22 17:03:31 +0530

    Modifying Welcome message

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,5 +1,8 @@
-public class EmpWageBuilde{
+public class EmpWageBuilder {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation");
+		int totalWorkingHrs=100;
+		for (i=0; i<=totalWorkingHrs; i++)
+			D
 		}
 	}	
