204c0bb1286fb61b7561c5d6bdfee2e71ef004ff
vegirevathi <vegirevathi@gmail.com>
2020-05-22 16:45:04 +0530

    Calculating Wages for month in UC5

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,5 +1,9 @@
 public class EmpWageBuilde{
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation");
+		int NumofDaysinMonth=20;
+		int WagesforMonth;
+		WagesforMonth=NumofDaysinMonth*DailyEmployeeWage;
+		System.out.println("Monthly Wage is" +WagesforMonth);
 		}
 	}	
