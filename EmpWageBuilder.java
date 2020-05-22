21c539fba49241f0e6b7dcd7f8954ff9577b7b13
vegirevathi <vegirevathi@gmail.com>
2020-05-22 15:52:10 +0530

    Calculating Daily Wage in UC2

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,5 +1,9 @@
-public class EmpWageBuilde{
+public class EmpWageBuilder {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation");
-		}
-	}	
+	int WagePerHr = 20, FullDayHrs = 8;
+	int DailyEmployeeWage;
+	DailyEmployeeWage = WagePerHr * FullDayHrs;
+	System.out.println("DailyEmployeeWage is" +DailyEmployeeWage);
+	}
+}
