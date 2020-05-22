e82b0ec1a39508fec04d5072c82749d0dce94217
vegirevathi <vegirevathi@gmail.com>
2020-05-22 16:36:10 +0530

    Solving Using Switch Case statement

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,5 +1,23 @@
-public class EmpWageBuilde{
+public class EmpWageBuilder {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation");
+		int WagePerHr=20, PartTimeHrs=4, FullDayHrs=8;
+		int DailyEmployeeWage;
+		int empCheck = (int)(Math.random() * 10) %2;
+		switch (empCheck) {
+		case 1:
+			System.out.println("daily Employee Wage for Full Time");
+			DailyEmployeeWage=WagePerHr*FullDayHrs;
+			break;
+		case 2:
+			System.out.println("Daily Employee Wage for Part Time");
+			DailyEmployeeWage=WagePerHr*PartTimeHrs;
+			break;
+		default :
+			System.out.println("Employee is absent");
+			DailyEmployeeWage=0;
+			break;
 		}
+		System.out.println("DailyEmployeeWage" +DailyEmployeeWage);
 	}	
+}
