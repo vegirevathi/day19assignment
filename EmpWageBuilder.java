0f4167562d4dcb8fdd4d757da252736adbd1b4a6
vegirevathi <vegirevathi@gmail.com>
2020-05-22 16:00:19 +0530

    Adding Part time Employee wage in UC3

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,5 +1,9 @@
-public class EmpWageBuilde{
+public class EmpWageBuilder {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation");
+		int WagePerHr=20, PartTimeHrs=4;
+		int DailyEmployeeWage;
+		DailyEmployeeWage=WagePerHr*PartTimeHrs;
+		System.out.println("DailyEmployeeWage" +DailyEmployeeWage);
 		}
 	}	
